bf805dc7494838a7854d124d975d6c69319f5485
Mohammed-Sufiyan <113841439+Mohammed-Sufiyan@users.noreply.github.com>
2022-11-06 09:15:36 +0530

    Delete Simple Projects directory

--- Simple Projects/Current time.py
+++
@@ -1,6 +0,0 @@
-import time
-#Finding the Seconds epoch
-seconds=time.time()
-print("Seconds since epoch=",seconds)
-local_time=time.ctime(seconds)
-print("Local time :", local_time)
--- Simple Projects/QR code.py
+++
@@ -1,9 +0,0 @@
-
-# QR codes are used to encode and decode the data into a machine-readable form.
-# The use of camera phones to read two-dimensional barcodes for various purposes
-
-import pyqrcode
-from pyqrcode import QRCode
-s = "www.google.com"
-URL = pyqrcode.create(s)
-URL.svg("google.svg", scale=8, background="grey")
